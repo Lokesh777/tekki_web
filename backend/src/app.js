67c11df8516e30155c265635c818280d7f7dfda5
Lokesh <lokesh@skizaa.com>
2026-08-30 18:46:33 +0530

    feat: Add cookie-parser for httpOnly cookie auth

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
 const config = require('./config/config');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/errorHandler');
@@ -52,6 +53,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
